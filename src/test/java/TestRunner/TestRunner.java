23290be1595450f27c991270436886b860b88d11
SdManoj26 <62830659+SdManoj26@users.noreply.github.com>
2024-06-21 11:12:05 +0530

    Update TestRunner.java

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,12 +1,12 @@
 package TestRunner;
  
-
+import org.junit.runner.RunWith;
 import org.testng.annotations.Test;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
  
-@Test
+@@RunWith(Cucumber.class)
 @CucumberOptions
 
 	(features = ".//Features/hackathon.feature" ,
@@ -19,6 +19,6 @@ import io.cucumber.testng.CucumberOptions;
 
 	)
 
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner {
  
 }
